023e8c467b8cba0a6f9016ecd4265e1d5efd28e4
Zumbrunnen Simon <Simon.Zumbrunnen@soudronic.com>
2020-03-04 13:11:22 +0100

    full screen figure.

--- frontend/src/app/components/app.js
+++ frontend/src/app/components/app.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useLayoutEffect } from 'react';
 import gql from 'graphql-tag';
 import { useSubscription } from '@apollo/react-hooks';
 import { ApiProvider } from '../../api/components/api-provider';
@@ -17,12 +17,22 @@ const TEST_SUBSCRIPTION = gql`
 
 function Test() {
     const { data } = useSubscription(TEST_SUBSCRIPTION);
+    const container = useRef();
+
     const figure = data ? data.scope.channels[0].waveform.figure : '';
 
+    useLayoutEffect(() => {
+        if (!(container.current && container.current.children[0])) {
+            return;
+        }
+        
+        container.current.children[0].setAttribute('height', '100%');
+        container.current.children[0].setAttribute('width', '100%');
+        container.current.children[0].setAttribute('preserveAspectRatio', 'none');
+    });
+
     return (
-        <div style={{
-            width: '100%'
-        }} dangerouslySetInnerHTML={{ __html: figure }} />
+        <div className='full-size' dangerouslySetInnerHTML={{ __html: figure }} ref={container} />
     );
 }
 
