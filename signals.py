26d0037a32b1df6bfa745e52b93a7d5d241c4f3d
Zumbrunnen Simon <Simon.Zumbrunnen@soudronic.com>
2020-05-20 16:48:33 +0200

    Test using qml backend for matplotlib.

--- signals.py
+++ signals.py
@@ -1,100 +1,30 @@
-import sys
+from sys import argv
 
-from numpy import linspace, sin, pi, float16
-
-import PyQt5.Qt as Qt
-from PyQt5.QtCore import pyqtProperty, QUrl
-from PyQt5.QtGui import QGuiApplication, QColor
+from PyQt5.QtCore import QObject, pyqtSlot
+from PyQt5.QtGui import QGuiApplication
 from PyQt5.QtQml import QQmlApplicationEngine, qmlRegisterType
-from PyQt5.QtQuick import QQuickItem, QSGNode, QSGGeometryNode, QSGGeometry, QSGFlatColorMaterial
 
-from plugins.demo_scope_plugin import DemoScopePlugin
+from matplotlib_backend_qtquick.backend_qtquickagg import FigureCanvasQtQuickAgg
 
-class Signal(QQuickItem):
-    def __init__(self, parent):
-        super().__init__(parent)
+class Test(QObject):
 
-        self._source = None
-
-        self.data_source_plugins = [
-            DemoScopePlugin(self)
-        ]
-
-        self.x_range = [0, 1]
-        self.y_range = [-1, 1]
-
-        self.n = 0
-        self.t = None
-        self.y = None
-
-        self.dirty = True
-
-        self.setFlag(QQuickItem.ItemHasContents, True)
-        self.geometry = QSGGeometry(QSGGeometry.defaultAttributes_Point2D(), self.n)
-        self.geometry.setDrawingMode(QSGGeometry.DrawLineStrip)
-        self.geometry.setLineWidth(2)
-        self.material = QSGFlatColorMaterial()
-        self.material.setColor(QColor(0, 0, 0))
-
-    @pyqtProperty(QUrl)
-    def source(self):
-        return self._source
-
-    @source.setter
-    def source(self, url):
-        for plugin in self.data_source_plugins:
-            if url.scheme() == plugin.scheme:
-                plugin.n_changed.connect(self.onNChanged)
-                plugin.t_changed.connect(self.onTChanged)
-                plugin.y_changed.connect(self.onYChanged)
-                plugin.start()
-            else:
-                plugin.stop()
-                plugin.n_changed.disconnect(self.onNChanged)
-                plugin.t_changed.disconnect(self.onTChanged)
-                plugin.y_changed.disconnect(self.onYChanged)
-
-        self._source = url
-
-    def onNChanged(self, n):
-        self.n = n
-        self.dirty = True
-        self.update()
-
-    def onTChanged(self, t):
-        self.x = t
-        self.dirty = True
-        self.update()
-
-    def onYChanged(self, y):
-        self.y = y
-        self.dirty = True
-        self.update()
-
-    def updatePaintNode(self, oldNode, updatePaintNodeData):
-        node = oldNode
-
-        if node == None or self.dirty:
-            if node == None:
-                node = QSGGeometryNode()
-                node.setGeometry(self.geometry)
-                node.setMaterial(self.material)
-
-            self.geometry.allocate(self.n)
-            points = self.geometry.vertexDataAsPoint2D()
-
-            if points:
-                for i, point in enumerate(points):
-                    point.set(self.x[i], -self.y[i])
-
-                node.markDirty(QSGNode.DirtyGeometry)
-
-            self.dirty = False
-
-        return node        
+    @pyqtSlot(FigureCanvasQtQuickAgg)
+    def func(self, canvas):
+        axis = canvas.figure.add_subplot('111')
+        axis.plot([1, 2, 3])
+        canvas.draw()
 
 if __name__ == "__main__":
-    app = QGuiApplication(sys.argv)
-    qmlRegisterType(Signal, 'Signals', 0, 1, 'Signal')
-    engine = QQmlApplicationEngine('signals.qml')
+    app = QGuiApplication(argv)
+    engine = QQmlApplicationEngine()
+
+    qmlRegisterType(FigureCanvasQtQuickAgg, 'Matplotlib', 0, 1, 'FigureCanvas')
+
+    test = Test()
+
+    context = engine.rootContext()
+    context.setContextProperty('test', test)
+
+    engine.load('signals.qml')
+
     exit(app.exec())
