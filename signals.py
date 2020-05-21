9a3c77161e5da1f140e6877543432d4c7aa09a3f
Zumbrunnen Simon <Simon.Zumbrunnen@soudronic.com>
2020-05-21 15:31:31 +0200

    Open CSV. Not working yet.

--- signals.py
+++ signals.py
@@ -1,18 +1,25 @@
 from sys import argv
 
-from PyQt5.QtCore import QObject, pyqtSlot
+from PyQt5.QtCore import QObject, QUrl, pyqtSlot
 from PyQt5.QtGui import QGuiApplication
 from PyQt5.QtQml import QQmlApplicationEngine, qmlRegisterType
 
 from matplotlib_backend_qtquick.backend_qtquickagg import FigureCanvasQtQuickAgg
 
-class Test(QObject):
+from numpy import genfromtxt
+
+class Controller(QObject):
+
+    @pyqtSlot(QUrl)
+    def open(self, url):
+        data = genfromtxt(url.toLocalFile(), delimiter=',')
+        axis = self.canvas.figure.add_subplot('111')
+        axis.plot(data[0], *data[1:])
+        self.canvas.draw()
 
     @pyqtSlot(FigureCanvasQtQuickAgg)
-    def func(self, canvas):
-        axis = canvas.figure.add_subplot('111')
-        axis.plot([1, 2, 3])
-        canvas.draw()
+    def setCanvas(self, canvas):
+        self.canvas = canvas
 
 if __name__ == "__main__":
     app = QGuiApplication(argv)
@@ -20,10 +27,10 @@ if __name__ == "__main__":
 
     qmlRegisterType(FigureCanvasQtQuickAgg, 'Matplotlib', 0, 1, 'FigureCanvas')
 
-    test = Test()
+    controller = Controller()
 
     context = engine.rootContext()
-    context.setContextProperty('test', test)
+    context.setContextProperty('controller', controller)
 
     engine.load('signals.qml')
 
--- signals.qml
+++ signals.qml
@@ -1,19 +1,47 @@
-import QtQuick.Window 2.2
 import QtQuick 2.14
+import QtQuick.Controls 2.4
+import QtQuick.Dialogs 1.0
+
 import Matplotlib 0.1
 
-Window {
+ApplicationWindow {
     visible: true
 
     width: 800
     height: 600
+
+    menuBar: MenuBar {
+        Menu {
+            title: qsTr('&File')
+            Action {
+                text: qsTr('&Open...')
+                onTriggered: {
+                    fileDialog.visible = true;
+                }
+            }
+        }
+    }
+
+    FileDialog {
+        id: fileDialog
+
+        nameFilters: [
+            'CSV files (*.csv *.txt)'
+        ]
+        selectMultiple: false
+        selectFolder: false
+
+        onAccepted: {
+            controller.open(fileDialog.fileUrl)
+        }
+    }
 
     FigureCanvas {
         id: canvas
         anchors.fill: parent
 
         Component.onCompleted: {
-            test.func(canvas)
+            controller.setCanvas(canvas)
         }
     }
 }
