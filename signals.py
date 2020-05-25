acebce5a0fd528b030ee28bf01e31310d999da6b
Zumbrunnen Simon <Simon.Zumbrunnen@soudronic.com>
2020-05-25 17:29:22 +0200

    Added treeview

--- signals.py
+++ signals.py
@@ -1,7 +1,8 @@
 from sys import argv
 
-from PyQt5.QtCore import QObject, pyqtProperty, pyqtSignal
-from PyQt5.QtWidgets import QMainWindow, QAction, QMenu, QApplication, QWidget, QFileDialog
+from PyQt5.Qt import Qt
+from PyQt5.QtCore import QObject, pyqtProperty, pyqtSignal, QUrl, QAbstractItemModel, QModelIndex
+from PyQt5.QtWidgets import QTreeWidget, QTreeWidgetItem, QMainWindow, QAction, QMenu, QDockWidget, QApplication, QWidget, QFileDialog
 
 from numpy import genfromtxt
 from matplotlib import use
@@ -14,24 +15,35 @@ class SignalsToolbar(NavigationToolbar):
     toolitems = [t for t in NavigationToolbar.toolitems if
                  t[0] in ('Home', 'Back', 'Forward', 'Pan', 'Zoom')]
 
-class Model(QObject):
+class Signal():
+    def __init__(self, t, y, name):
+        self.t = t
+        self.y = y
+        self.name = name
+
+class Measurement():
     pass
 
-class EmptyModel(Model):
-    pass
+class FileMeasurement(Measurement):
+    scheme = 'file://'
 
-class CSVModel(Model):
-    def __init__(self, fileName):
-        super().__init__()
-        self.fileName = fileName
-        self.data = genfromtxt(fileName, delimiter=',').T
+    def __init__(self, url):
+        self.url = url
+        data = genfromtxt(self.url.toLocalFile(), delimiter=',').T
+
+        self.signals = []
+
+        t = data[0]
+
+        for i, y in enumerate(data[1:]):
+            self.signals.append(Signal(t, y, f'Signal {i}'))
 
 class SignalsApplication(QApplication):
     def __init__(self):
         super().__init__(argv)
 
         initialWindow = SignalsWindow(self)
-        initialWindow.model = EmptyModel()
+        initialWindow.measurement = None
 
         self.windows = [
             initialWindow
@@ -40,24 +52,26 @@ class SignalsApplication(QApplication):
         initialWindow.show()
 
     def open(self, window, fileName):
+        url = QUrl.fromLocalFile(fileName)
+        
         for window in self.windows:
             # If the file is already open, focus the window
-            if type(window.model) == CSVModel and window.model.fileName == fileName:
+            if type(window.measurement) == FileMeasurement and window.measurement.url == url:
                 window.activateWindow()
                 break
         else:
             # If the window is empty, use this window...
-            if type(window.model) == EmptyModel:
-                window.model = CSVModel(fileName)
+            if not window.measurement:
+                window.measurement = FileMeasurement(url)
             # else create a new window
             else:
                 newWindow = SignalsWindow(self)
-                newWindow.model = CSVModel(fileName)
+                newWindow.measurement = FileMeasurement(url)
                 newWindow.show()
                 self.windows.append(newWindow)
 
 class SignalsWindow(QMainWindow):
-    modelChanged = pyqtSignal()
+    measurementChanged = pyqtSignal()
 
     def __init__(self, app):
         super().__init__()
@@ -76,26 +90,39 @@ class SignalsWindow(QMainWindow):
         self.addToolBar(toolBar)
 
         self.setCentralWidget(self.figureCanvas)
-        self.modelChanged.connect(self.onModelChanged)
+
+        self.treeView = QTreeWidget()
+        dockWidget = QDockWidget('Signals')
+        self.addDockWidget(Qt.DockWidgetArea.LeftDockWidgetArea, dockWidget)
+        dockWidget.setWidget(self.treeView)
+
+        self.measurementChanged.connect(self.onMeasurementChanged)
 
     def open(self):
         fileName, _ = QFileDialog.getOpenFileName(self)
         self.app.open(self, fileName)
     
-    @pyqtProperty(Model, notify=modelChanged)
-    def model(self):
-        return self._model
+    @pyqtProperty(Measurement, notify=measurementChanged)
+    def measurement(self):
+        return self._measurement
 
-    @model.setter
-    def model(self, model):
-        self._model = model
-        self.modelChanged.emit()
+    @measurement.setter
+    def measurement(self, measurement):
+        self._measurement = measurement
+        self.measurementChanged.emit()
 
-    def onModelChanged(self):
-        if type(self.model) == EmptyModel:
+    def onMeasurementChanged(self):
+        if not self.measurement:
             return
 
-        self.axis.plot(self.model.data[0], *self.model.data[1:])
+        root = QTreeWidgetItem([self.measurement.url.fileName()])
+
+        for signal in self.measurement.signals:
+            self.axis.plot(signal.t, signal.y)
+            root.addChild(QTreeWidgetItem([signal.name]))
+
+        self.treeView.addTopLevelItem(root)
+
         self.figureCanvas.draw()
 
 def main():
