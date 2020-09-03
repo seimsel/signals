a92ba89442e5d2775af82ec7d7f1e7cd62be8200
Zumbrunnen Simon <Simon.Zumbrunnen@soudronic.com>
2020-09-03 11:00:40 +0200

    No borders on image.

--- signals-ui/src/figure/components/figure.js
+++ signals-ui/src/figure/components/figure.js
@@ -3,6 +3,10 @@ import React from 'react';
 export function Figure() {
     return (
         <img
+            style={{
+                width: '100%',
+                height: '100%'
+            }}
             src={ `${process.env.SERVER_HTTP_URL}/figure` }
         />
     );
